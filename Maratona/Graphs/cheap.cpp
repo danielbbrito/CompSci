29b5b42fdf1a5c2572ed94dff1ee40e73b3268a7
NoneCoding <danieldebrito2105@gmail.com>
2023-03-27 23:05:44 -0300

    commit

--- Maratona/Graphs/cheap.cpp
+++ Maratona/Graphs/cheap.cpp
@@ -10,33 +10,34 @@ void bfs(string s, string e)
     for (auto it: adjlist)
         seen[it.first] = false;
 
-    queue<vector<pair<string, string>>> q;
+    queue<vector<string>> q;
 
     seen[s] = true;
-    seen[adjlist[s].front()] = true;
 
-    q.push({{s, adjlist[s].front()}});
+    q.push({s});
 
     while(q.size())
     {
-       vector<pair<string, string>> curr = q.front();
+       vector<string> curr = q.front();
        q.pop();
 
-       if (curr.back().second == e)
+       if (curr.back() == e)
        {
-            for (int i = 0; i < curr.size(); i++)
-                cout << curr[i].first << ' ' << curr[i].second << endl;
+            for (int i = 0; i < curr.size() - 1; i++)
+                cout << curr[i] << ' ' << curr[i + 1] << endl;
+                
             
             return;
        }
 
-       for (auto it: adjlist[curr.back().second])
+       for (auto it: adjlist[curr.back()])
             if (!seen[it])
             {
                 seen[it] = true;
-                curr.push_back({curr.back().second, it});
+                vector<string> next = curr;
+                next.push_back(it);
 
-                q.push(curr);
+                q.push(next);
             }
 
     }
@@ -45,6 +46,7 @@ void bfs(string s, string e)
     return;
     
 }
+
 int main()
 {
     int n;
@@ -67,4 +69,6 @@ int main()
         cout << endl;
         adjlist.clear();
     }
+
+    return 0;
 }
