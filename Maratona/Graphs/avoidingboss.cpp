0ac7cf3682744d2bab40cc6de571e556a0d880fc
NoneCoding <danieldebrito2105@gmail.com>
2023-11-02 11:00:18 -0300

    commit

--- Maratona/Graphs/avoidingboss.cpp
+++ Maratona/Graphs/avoidingboss.cpp
@@ -3,10 +3,46 @@
 using namespace std;
 #define INF 1000000000
 int am[105][105];
-
-int fw(int s, int e)
+int p[105][105];
+int fw(int s, int e, int mx)
 {
+    int cpy[105][105];
 
+    for (int i = 1; i < 105; i++)
+        for (int j = 1; j < 105; j++)
+        {
+            cpy[i][j] = am[i][j];
+            p[i][j] = i;
+        }
+
+    // Actual algo
+    for (int k = 1; k <= mx; k++)
+    {
+        for (int i = 1; i <= mx; i++)
+        {
+            for (int j = 1; j <= mx; j++)
+            {
+                if (cpy[i][j] > cpy[i][k] + cpy[k][j])
+                {
+                    cpy[i][j] = cpy[i][k] + cpy[k][j];
+                    p[i][j] = p[k][j];
+                }
+            }
+        }
+    }
+
+    return cpy[s][e];
+}
+
+void remove(int i, int j, int ant)
+{
+    if (i != j)
+    {
+        remove(i, p[i][j], j);
+    }
+
+    am[ant][j] = INF;
+    am[j][ant] = INF;
 }
 
 int main()
@@ -15,6 +51,11 @@ int main()
 
     while (cin >> p >> r >> bh >> of >> yh >> m)
     {
+        for (int i = 1; i < 105; i++)
+            for (int j = 1; j < 105; j++)
+                if (i != j)
+                    am[i][j] = INF;
+
         for (int i = 0; i < r; i++)
         {
             int a, b, d;
@@ -23,6 +64,22 @@ int main()
             am[b][a] = d;
         }
 
+        if (am[bh][of] != INF)
+            am[bh][of] = INF;
+        int inter = fw(bh, of, p);
 
+        while (inter != INF && inter)
+        {
+            remove(bh, of, bh);
+            inter = fw(bh, of, p);
+        }
+
+        inter = fw(yh, m, p);
+
+        if (inter == INF || yh == m || of == m || bh == m)
+            cout << "MISSION IMPOSSIBLE.\n";
+
+        else
+            cout << inter << endl;
     }
 }
--- Maratona/Graphs/pagehopping.cpp
+++ Maratona/Graphs/pagehopping.cpp
@@ -5,10 +5,6 @@ using namespace std;
 int main()
 {
     int am[105][105];
-    for (int i = 0; i < 105; i ++)
-        for (int j = 0; j < 105; j++)
-            am[i][j] = 1000000000;
-
 
     int a, b, t = 1;
     cin >> a >> b;
@@ -16,7 +12,15 @@ int main()
     while (a && b)
     {
         int mx = max(a, b);
-        float count = 1.0;
+        set<int> seen;
+        for (int i = 0; i < 105; i++)
+            for (int j = 0; j < 105; j++)
+                if (i != j)
+                    am[i][j] = 1000000000;
+                else
+                    am[i][j] = 0;
+                    
+    
         am[a][b] = 1;
 
         cin >> a >> b;
@@ -24,20 +28,35 @@ int main()
         while (a || b)
         {
             mx = max(max(a, b), mx);
-            count++;
+            if (seen.find(a) == seen.end()) seen.insert(a);
+            if (seen.find(b) == seen.end()) seen.insert(b);
+
             am[a][b] = 1;
             cin >> a >> b;
         }
 
+        int sz = seen.size();seen.clear();
         for (int k = 1; k <= mx; k++)
+        {
             for (int i = 1; i <= mx; i++)
+            {
                 for (int j = 1; j <= mx; j++)
+                {
+                    //cout << "For nodes " << i <<" and " << j << " am[i][j] is " << am[i][j] << " am[i][k] is " << am[i][k] << " and am[k][j] is " << am[k][j] << endl;
                     am[i][j] = min(am[i][j], am[i][k] + am[k][j]);
+                }
+
+                
+            }
+            
+        }
+        float count = (float)sz * (sz - 1);
 
         int sum = 0;
         for (int i = 1; i <= mx; i++)
             for (int j = 1; j <= mx; j++)
-                sum += am[i][j];
+                if (am[i][j] < 1000000000)
+                    sum += am[i][j];
 
         cout << fixed << setprecision(3) << "Case " << t << ": average length between pages = " << sum / count  << " clicks\n";
         cin >> a >> b;
