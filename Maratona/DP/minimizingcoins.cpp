f96f7675f6b275618470660db99fb610dc0ce357
Daniel de Brito <danieldebrito@Daniels-MacBook-Air.local>
2024-03-07 11:36:09 -0300

    commit

--- Maratona/DP/minimizingcoins.cpp
+++ Maratona/DP/minimizingcoins.cpp
@@ -2,21 +2,23 @@
 using namespace std;
 int n, x;
 vector<int> coins;
-
+vector<int> value;
+vector<bool> ready;
 int solve(int sum)
 {
-    if (sum < 0) return INT_MAX;
-    if (sum == 0)
-    {
-        return 0;
-    }
-    int best = INT_MAX;
+    if (sum < 0) return 1e9;
+
+    if (sum == 0) return 0;
+    if (ready[sum]) return value[sum];
+    int best = 1e9;
 
     for (auto c: coins)
     {
         best = min(best, solve(sum - c) + 1);
     }
     
+    ready[sum] = true;
+    value[sum] = best;
     return best;
 }
 int main()
@@ -29,7 +31,9 @@ int main()
 
         coins.push_back(a);
    }
-
+    
+    value.assign(x + n + 1, 0);
+    ready.assign(x + n + 1, false);
    int ans = solve(x);
 
 
