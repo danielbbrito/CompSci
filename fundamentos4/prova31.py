ab33bffc679b5ea94a59152b92c7438a7168f70e
Daniel de Brito <danieldebrito2105@gmail.com>
2024-05-09 18:58:25 -0300

    Interpolação polinômio de lagrange

--- fundamentos4/prova31.py
+++ fundamentos4/prova31.py
@@ -1,12 +1,12 @@
 import math
 
-def polinomio_lagrange(m: int, x: list, y: list, z: float) -> float:
+def polinomio_lagrange(x: list, y: list, z: float) -> float:
     r = 0
 
-    for i in range(m):
+    for i in range(1, 5): # Tomando apenas os n + 1 pontos mais próximos
         c = 1
         d = 1
-        for j in range(m):
+        for j in range(1, 5):
             if i != j:
                 c = c * (z - x[j])
                 d = d * (x[i] - x[j])
@@ -18,12 +18,12 @@ def polinomio_lagrange(m: int, x: list, y: list, z: float) -> float:
 
 
 def main():
-    m = 3
+    m = 7
     z = 1.4
     x = [0.7, 1.2, 1.3, 1.5, 2, 2.3, 2.6]
     y = [0.043, 1.928, 2.497, 3.875, 9, 13.467, 19.176]
 
-    r = polinomio_lagrange(m, x, y, z)
+    r = polinomio_lagrange(x, y, z)
 
     print(r)
 
